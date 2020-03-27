edd901268a0033b9886bae7b02aa2d496ad46269
Tiago Martins <vargas.tiago@hotmail.com>
2020-03-26 21:48:28 -0300

    Created the mobile version of the aplication

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
